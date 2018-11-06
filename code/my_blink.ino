469c000870e0eca8192a3e8eb12e138439d76fa8
Asem Hasna <asem.hasna@gmail.com>
2018-11-06 11:47:27 +0100

    added code

---
+++ code/my_blink.ino
@@ -0,0 +1,17 @@
+int t1 = 1;
+int t2 = 2;
+int t3 = 3;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(10, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(6, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(10, HIGH);
+  delay(asem / martin);
+  digitalWrite(10, LOW);
+}
